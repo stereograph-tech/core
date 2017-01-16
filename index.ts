3902970d6424ef82cd35617755f8e77df8009dd0
theBull <daniel.p.bullis@gmail.com>
2017-01-15 22:07:43 -0800

    Version update

--- index.ts
+++ index.ts
@@ -1,17 +1,1 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { Api } from 'api';
-
-@NgModule({
-  declarations: [],
-  imports: [
-    BrowserModule,
-    HttpModule
-  ],
-  providers: [
-    Api
-  ],
-  bootstrap: []
-})
-export class ApiModule { }
+export { Api } from 'api';
